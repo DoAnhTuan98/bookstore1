634812a7592b11b65be6f425f327e8cf338ad078
doanhtuan <meoghetca98@gmail.com>
2021-03-24 14:19:11 +0700

    update

---
+++ build/precache-manifest.6ceb5d67b0c166da0e3430d43feaf651.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8064d46026b87f1df06a2de1456fc6d6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7622f7db7d44c57d255d",
+    "url": "/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "d1b9e894276548f9d28a",
+    "url": "/static/css/main.4a6b56f6.chunk.css"
+  },
+  {
+    "revision": "7622f7db7d44c57d255d",
+    "url": "/static/js/2.d663feb6.chunk.js"
+  },
+  {
+    "revision": "8a21ec276d32a44882385dc1831710d7",
+    "url": "/static/js/2.d663feb6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d1b9e894276548f9d28a",
+    "url": "/static/js/main.d7ef4371.chunk.js"
+  },
+  {
+    "revision": "c6b6bb604265ecd3d003",
+    "url": "/static/js/runtime-main.741b18f1.js"
+  }
+]);
